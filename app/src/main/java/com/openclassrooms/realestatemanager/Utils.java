4651b9aed6e46fdf3fed2851cee0f03363df5804
Brice <andro.dev.work@gmail.com>
2020-12-09 23:06:59 +0100

    Upgrade and add methods

--- app/src/main/java/com/openclassrooms/realestatemanager/Utils.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utils.java
@@ -1,11 +1,16 @@
 package com.openclassrooms.realestatemanager;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
 import android.net.wifi.WifiManager;
+import android.util.Log;
 
+import java.net.InetAddress;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Philippe on 21/02/2018.
@@ -16,31 +21,61 @@ public class Utils {
     /**
      * Conversion d'un prix d'un bien immobilier (Dollars vers Euros)
      * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     *
      * @param dollars
      * @return
      */
-    public static int convertDollarToEuro(int dollars){
+    public static int convertDollarToEuro(int dollars) {
         return (int) Math.round(dollars * 0.812);
+    }
+
+    //New method
+    public static int convertEuroToDollar(int euros) {
+        return (int) Math.round(euros * 1.212);
     }
 
     /**
      * Conversion de la date d'aujourd'hui en un format plus approprié
      * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     *
      * @return
      */
-    public static String getTodayDate(){
+    public static String getTodayDate1() {
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        return dateFormat.format(new Date());
+    }
+
+    //New method
+    public static String getTodayDate2() {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         return dateFormat.format(new Date());
     }
 
     /**
      * Vérification de la connexion réseau
      * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     *
      * @param context
      * @return
      */
-    public static Boolean isInternetAvailable(Context context){
-        WifiManager wifi = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+    public static Boolean isInternetAvailable1(Context context) {
+        WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
         return wifi.isWifiEnabled();
+    }
+
+    //New method
+    public static Boolean isInternetAvailable2(Context context) {
+//        try {
+//            InetAddress ipAddr = InetAddress.getByName("google.com");
+//            return !ipAddr.equals("");
+//
+//        } catch (Exception e) {
+//            Log.e("Utils", "isInternetAvailable2: ", e);
+//            return false;
+//        }
+
+        ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+
     }
 }
