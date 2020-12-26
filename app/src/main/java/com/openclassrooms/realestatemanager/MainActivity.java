c00d66ecb6d2317d7ffd648e3c9d74b0712e815d
Brice <andro.dev.work@gmail.com>
2020-12-26 19:51:36 +0100

    Update viewpager

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -1,17 +1,11 @@
 package com.openclassrooms.realestatemanager;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static androidx.fragment.app.FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT;
 
--- app/src/main/java/com/openclassrooms/realestatemanager/ViewPagerAdapter.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/ViewPagerAdapter.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 public class ViewPagerAdapter extends FragmentPagerAdapter {
+    public static final int NUMBER_FRAGMENT = 2;
 
     public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
         super(fm, behavior);
@@ -15,17 +16,16 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
     @Override
     public Fragment getItem(int position) {
         switch (position) {
-            case 0:
-                return new MapFragment();
             case 1:
                 return new RVListRealEstateFragment();
+            case 0:
             default:
-                return null;
+                return new MapFragment();
         }
     }
 
     @Override
     public int getCount() {
-        return 2;
+        return NUMBER_FRAGMENT;
     }
 }
