bc2b28eee360ff0a61627a0cea7021fdc78ab372
Brice <andro.dev.work@gmail.com>
2021-01-08 18:12:05 +0100

    Add unit and instrumented tests

---
+++ app/src/test/java/com/openclassrooms/realestatemanager/UtilsUnitTest.java
@@ -0,0 +1,33 @@
+package com.openclassrooms.realestatemanager;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+public class UtilsUnitTest {
+
+    @Test
+    public void convertDollarToEuro() {
+        assertEquals(81, Utils.convertDollarToEuro(100));
+    }
+
+    @Test
+    public void convertEuroToDollar() {
+        assertEquals(121.2, Utils.convertEuroToDollar(100), 1);
+    }
+
+    @Test
+    public void getTodayDate2() {
+        final Date date = Mockito.mock(Date.class);
+        Mockito.when(date.getTime()).thenReturn(new Date().getTime());
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String toto = dateFormat.format(date.getTime());
+
+        assertEquals(toto, Utils.getTodayDate2());
+    }
+}
