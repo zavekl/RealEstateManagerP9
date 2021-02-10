e844c66da450d0a62081e16906f1ab33bd92b35e
Brice-NIATEL <andro.dev.work@gmail.com>
2021-02-10 19:59:47 +0100

    Create Floating Action Button

--- app/src/main/java/com/openclassrooms/realestatemanager/fragment/RVListRealEstateFragment.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/fragment/RVListRealEstateFragment.java
@@ -13,9 +13,12 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.openclassrooms.realestatemanager.R;
+import com.openclassrooms.realestatemanager.activity.MainActivity;
 import com.openclassrooms.realestatemanager.adapter.ListRealEstateRVAdapter;
 import com.openclassrooms.realestatemanager.model.RealEstate;
+import com.openclassrooms.realestatemanager.utils.VerticalSpaceItemDecoration;
 import com.openclassrooms.realestatemanager.viewmodel.RVListRealEstateViewModel;
 
 import java.util.List;
@@ -23,11 +26,15 @@ import java.util.List;
 public class RVListRealEstateFragment extends Fragment {
     private ListRealEstateRVAdapter mAdapter;
 
+    private FloatingActionButton mFAB;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_rv_real_estate, container, false);
+        View view = inflater.inflate(R.layout.rv_real_estate_fragment, container, false);
+
+        mFAB = view.findViewById(R.id.rv_fab);
+        return view;
     }
 
     @Override
@@ -43,7 +50,6 @@ public class RVListRealEstateFragment extends Fragment {
                 mAdapter.setItems(realEstates);
             }
         });
-
     }
 
     @Override
@@ -51,6 +57,39 @@ public class RVListRealEstateFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         RecyclerView mRecyclerView = view.findViewById(R.id.rv_real_estate);
         mAdapter = new ListRealEstateRVAdapter(requireActivity(), this);
+        mRecyclerView.addItemDecoration(new VerticalSpaceItemDecoration(15));
         mRecyclerView.setAdapter(mAdapter);
+
+        onScrollRecyclerView(mRecyclerView);
+
+        onClickFAB();
+    }
+
+    //On click of FAB run AddRealEstateFragment
+    private void onClickFAB() {
+        mFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = AddRealEstateFragment.newInstance();
+                getParentFragmentManager().beginTransaction().setReorderingAllowed(true)
+                        .add(R.id.description_fragment, fragment, null)
+                        .commit();
+                MainActivity.hideViewPager();
+            }
+        });
+    }
+
+    //Make fab disapear if scroll down the RV
+    private void onScrollRecyclerView(RecyclerView recyclerView) {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (dy > 0)
+                    mFAB.hide();
+                else if (dy < 0)
+                    mFAB.show();
+            }
+        });
     }
 }
