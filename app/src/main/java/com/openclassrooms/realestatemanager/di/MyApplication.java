aabcf138fdc4c080c215f6fbba4f854af5c1f96d
Brice-NIATEL <andro.dev.work@gmail.com>
2021-02-25 15:29:20 +0100

    Fix crash multidex plus delete a library and modify manifest

--- app/src/main/java/com/openclassrooms/realestatemanager/di/MyApplication.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/di/MyApplication.java
@@ -1,7 +1,10 @@
 package com.openclassrooms.realestatemanager.di;
 
 import android.app.Application;
+import android.content.Context;
 import android.util.Log;
+
+import androidx.multidex.MultiDex;
 
 /**
  * Created by <NIATEL BRICE> on <16/04/2020>.
@@ -9,6 +12,13 @@ import android.util.Log;
 public class MyApplication extends Application {
     private static final String TAG = "MyApplication";
     private ContainerDependencies containerDependencies;
+
+    //Enable multidex for version <=19
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
 
     public ContainerDependencies getContainerDependencies() {
         if (containerDependencies == null) {
