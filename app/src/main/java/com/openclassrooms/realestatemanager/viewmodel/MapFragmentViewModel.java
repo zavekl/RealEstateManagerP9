a7337029ca3f092d68dfe533dd79bed8da6f05d2
Brice-NIATEL <andro.dev.work@gmail.com>
2021-03-02 15:49:10 +0100

    Manage the location of user

--- app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/MapFragmentViewModel.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/MapFragmentViewModel.java
@@ -1,7 +1,32 @@
 package com.openclassrooms.realestatemanager.viewmodel;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
+import android.location.LocationListener;
 
-public class MapFragmentViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.google.android.gms.location.LocationCallback;
+import com.openclassrooms.realestatemanager.di.MyApplication;
+import com.openclassrooms.realestatemanager.repository.MapRepository;
+
+public class MapFragmentViewModel extends AndroidViewModel {
+    private final MapRepository mMapRepository;
+
+    public MapFragmentViewModel(@NonNull Application application) {
+        super(application);
+        mMapRepository = ((MyApplication) application).getContainerDependencies().getMapRepository();
+    }
+
+    public void startLocationUpdates(LocationCallback locationCallback) {
+        mMapRepository.startLocationUpdates(locationCallback);
+    }
+
+    public void stopLocationUpdates(LocationCallback locationCallback) {
+        mMapRepository.stopLocationUpdates(locationCallback);
+    }
+
+    public void requestGPSUpdate(LocationListener locationListener) {
+        mMapRepository.requestGPSUpdate(locationListener);
+    }
 }
